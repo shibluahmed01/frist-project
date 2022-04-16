4d50a366b6a578f42523e6437e7983b2999e93ba
shibluahmed01 <mdshibluahmed01@gmail.com>
2022-04-16 01:29:22 -1200

    fristproject

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+        <button @click="godBoy='compHome'">Home</button>
+        <button @click="godBoy='compAbout'">About</button>
+        <button @click="godBoy='compContact'">Contact</button>
+        
+
+        <keep-alive>
+            <component :is="godBoy"></component>
+        </keep-alive>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+import compHome from './Components/HomeView.vue'
+import compAbout from './Components/AboutView.vue'
+import compContact from './Components/ContactView.vue'
+export default{
+    data(){
+        return{
+            godBoy: compHome
+        }
+    },
+    components: {
+        compHome,
+        compAbout,
+        compContact
+    }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
